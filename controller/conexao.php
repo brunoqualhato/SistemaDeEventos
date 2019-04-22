28323458014716d8c4d51e9563e5f01d8f782d44
Bruno Qualhato <brunoqualhato1@gmail.com>
2019-04-22 09:56:31 -0300

    Update conexao.php

--- controller/conexao.php
+++ controller/conexao.php
@@ -18,9 +18,10 @@ class Conexao
     {
         if (is_null(self::$conexao)) {
             //alterar a senha e o usuario conforme necessario
-            self::$conexao = new \PDO('mysql:host=localhost;port=3306;dbname=ifgcc_semana_academica', 'root', '1');
-            self::$conexao->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-            self::$conexao->exec('set names utf8');
+            self::$conexao = new \PDO('mysql:host=localhost;port=3306;dbname=ifgcc_semana_academica', 'root', '1',
+                                      array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            self::$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$conexao->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
         }
         return self::$conexao;
     }
